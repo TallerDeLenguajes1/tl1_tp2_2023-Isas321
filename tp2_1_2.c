c16d700ed1647b650c7324a8a690f0882fa86401
Isas321 <javier.isas321@gmail.com>
2023-03-25 22:44:21 -0300

    Recorrido de vecotr con puntero

---
+++ tp2_1_2.c
@@ -0,0 +1,19 @@
+// codigo implementado con punteros
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){  
+    #define N 20
+    int i;
+    double vt[N];
+    double *puntero;
+
+    puntero=vt;     //Guardo la direccion de memoria del primer elemento del vector, direccion de v[0]
+
+    for(i = 0;i<N; i++)
+    {
+        *(puntero+i)=1+rand()%100;                      //Guardo el valor de rand en "vt[0+i]"                          
+        printf("\nvt[%d]: %f ", i, *(puntero+i));       //Muestra el valor que hay en el vector en la direccion almacenada por el puntero + i, es decir muestra los que hay en "vt[0+i]"
+    }
+}
