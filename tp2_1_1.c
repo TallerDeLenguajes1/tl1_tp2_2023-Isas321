504758cdd86b6ae3a288a65d67eb80b43d5e83f9
Isas321 <javier.isas321@gmail.com>
2023-07-22 13:24:50 -0300

    Observaciones varias

--- tp2_1_1.c
+++ tp2_1_1.c
@@ -11,6 +11,9 @@ int main(){
     for(i = 0;i<N; i++)
     {
         vt[i]=1+rand()%100;         //Para usar la funcion rand es necesario llamar a la biblioteca stdlib.h
-        printf("%f ", vt[i]);
+        // La funcion rand()%100 nos entrega un numero entre 0 y 99
+        // Es por ese motivo que le sumamos 1 en el codigo vt[i]=1+rand()%100;
+        // Y asi tendremos valores 1-100
+        printf("%.2f ", vt[i]); //Para mostrar dos decimales pongo .2f
     }
 }
